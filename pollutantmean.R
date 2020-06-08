b4ed9eb56f7dc36541a51bd6a2178c776a198db1
Tan Nguyen <tan.nguyen2@halliburton.com>
2020-06-07 22:36:44 -0500

    finish the exercise

--- pollutantmean.R
+++ pollutantmean.R
@@ -13,6 +13,20 @@ pollutantmean <- function(directory, pollutant, id = 1:332){
   mean(pollutantData)
 }
 
+complete <- function(directory, id = 1:332){
+  result <- data.frame()
+  result <- rbind(result, c("id", "nobs"))
+  for (i in id) {
+    name <- nameById(i)
+    filePath <- file.path(directory, name)
+    csv <- read.csv(filePath, comment.char = "")
+    nobs <- nrow(csv[complete.cases(csv), ])
+    
+    result<- rbind(result, c(i, nobs))
+  }
+  result
+}
+
 nameById <- function(id){
   if (id < 10) {
     name <- paste("00", id, ".csv", sep = "")

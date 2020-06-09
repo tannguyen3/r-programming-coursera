1f3c3d88e4f6be50ddb50a74815895668b9d3ec2
Tan Nguyen <tan.nguyen2@halliburton.com>
2020-06-09 17:35:25 -0500

    part 4

--- Week 4/assignment3.R
+++ Week 4/assignment3.R
@@ -40,9 +40,9 @@ best <- function(state, outcome){
   sort(lowestRateHospitals)[1]
 }
 
-#Test: rankhospital("MN", "heart attack", 5000) => NA
-#Test: rankhospital("MD", "heart attack", "worst")  => "HARFORD MEMORIAL HOSPITAL"
-#Test: rankhospital("TX", "heart failure", 4) => "DETAR HOSPITAL NAVARRO"
+#Test: rankhospital("MN", "heart attack", 5000) # NA
+#Test: rankhospital("MD", "heart attack", "worst")  # "HARFORD MEMORIAL HOSPITAL"
+#Test: rankhospital("TX", "heart failure", 4) # "DETAR HOSPITAL NAVARRO"
 rankhospital <- function(state, outcome, num = "best"){
   # get the outcome column from outcome 
   outcomeCol <- getOutcomeCol(outcome)
@@ -51,13 +51,63 @@ rankhospital <- function(state, outcome, num = "best"){
   stateData <- readStateData(state, outcomeCol)
   ranking <- stateData[order(stateData$Rate, stateData$Hospital.Name), ]
   
+  num <- convertNumToRankingFunc(num)(ranking)
+  
+  ranking[num, 1]
+}
+
+#head(rankall("heart attack", 20), 10)
+# tail(rankall("pneumonia", "worst"), 3)
+
+rankall <- function(outcome, num = "best"){
+  # get the outcome column from outcome 
+  outcomeCol <- getOutcomeCol(outcome)
+  hospitalNameCol <- 2
+  stateCol <- 7
+  outcomeData <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+  
+  outcomeData <- outcomeData[, c(stateCol, hospitalNameCol, outcomeCol) ]
+  Rate <- as.numeric(outcomeData[, 3])
+  outcomeData <- cbind(outcomeData, Rate)
+  outcomeData <- outcomeData[which(complete.cases(outcomeData) ), ]
+  
+  ranking <- outcomeData[order(outcomeData$State, outcomeData$Rate, outcomeData$Hospital.Name), ]
+  
+  split <- split(ranking, ranking$State)
+  convertFunc <- convertNumToRankingFunc(num)
+  rankingByState <- lapply(split, function(x) x[convertFunc(x), ])
+  
+  result <- data.frame()
+  
+  
+  for (item in rankingByState) {
+    result <- rbind(result, c(item$Hospital.Name, item$State))
+  }
+  colnames(result) <- c("hospital", "state")
+  result
+}
+
+convertNumToRankingFunc <- function(num){
+  convertFunc <- function(data){
+    if (num == "best") {
+      num <- 1
+    } else if (num == "worst") {
+      num <- nrow(data)
+    } else if (!is.numeric(num)) {
+      stop("invalid num")
+    }
+    num
+  }
+  convertFunc
+}
+
+convertNumToRanking <- function(num, data){
   if (num == "best") {
     num <- 1
   } else if (num == "worst") {
-    num <- nrow(ranking)
+    num <- nrow(data)
   } else if (!is.numeric(num)) {
     stop("invalid num")
   }
-  
-  ranking[num, 1]
+  num
 }
